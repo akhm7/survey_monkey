29f66a476933ae7d8acc3c934cff7de6ed150738
Khojiakbar Akhmedov <xgm.suite@gmail.com>
2023-11-14 00:59:21 +0500

    add general models
    user, survey, question, answer

---
+++ db/models.py
@@ -0,0 +1,52 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Enum
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy.sql import func
+from enum import Enum
+
+Base = declarative_base()
+
+class QuestionType(Enum):
+    TEXT = 'text'
+    SINGLE_CHOICE = 'single_choice'
+    MULTIPLE_CHOICE = 'multiple_choice'
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password_hash = Column(String)
+
+class Survey(Base):
+    __tablename__ = 'surveys'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    start_date = Column(DateTime(timezone=True), server_default=func.now())
+    end_date = Column(DateTime(timezone=True))
+    created_by = Column(Integer, ForeignKey('users.id'))
+
+    questions = relationship('Question', back_populates='survey')
+
+class Question(Base):
+    __tablename__ = 'questions'
+
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String)
+    question_type = Column(Enum(QuestionType))
+    survey_id = Column(Integer, ForeignKey('surveys.id'))
+
+    survey = relationship('Survey', back_populates='questions')
+    answers = relationship('Answer', back_populates='question')
+
+class Answer(Base):
+    __tablename__ = 'answers'
+
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    question_id = Column(Integer, ForeignKey('questions.id'))
+
+    user = relationship('User')
+    question = relationship('Question', back_populates='answers')
